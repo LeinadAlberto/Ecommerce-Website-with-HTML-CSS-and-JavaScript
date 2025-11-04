52fff13640b9f6c614e1c3da8487988d841ceb95
Alberto <dany.canaviri2022@gmail.com>
2025-11-03 23:58:26 -0400

    Home section, completed

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,10 @@
 /*=============== HOME SWIPER ===============*/
-
+var HomeSwiper = new Swiper(".home-swiper", {
+	loop: true, 
+	pagination: {
+		el: ".swiper-pagination",
+	},
+});
 /*=============== PRODUCT SWIPER ===============*/
 
 /*=============== ELEVATE SWIPER ===============*/
